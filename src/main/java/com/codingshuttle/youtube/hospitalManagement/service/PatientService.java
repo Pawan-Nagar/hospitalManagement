20a09a10a40f109d0051af59adfbb8a1f55df69c
pawan <pa211nagar@gmail.com>
2025-07-25 20:35:19 +0530

    testing and add more feature to the project

--- src/main/java/com/codingshuttle/youtube/hospitalManagement/service/PatientService.java
+++ src/main/java/com/codingshuttle/youtube/hospitalManagement/service/PatientService.java
@@ -3,7 +3,11 @@ package com.codingshuttle.youtube.hospitalManagement.service;
 
 import com.codingshuttle.youtube.hospitalManagement.entity.Patient;
 import com.codingshuttle.youtube.hospitalManagement.repository.PatientRepository;
+import jakarta.transaction.Transactional;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,10 +16,22 @@ public class PatientService {
 
     private final PatientRepository patientRepository;
 
-    public Patient getPatientById(Long id){
-        Patient p1 = patientRepository.findById(id).orElseThrow();
 
-        Patient p2 = patientRepository.findById(id).orElseThrow();
+    @Autowired
+    public PatientService(PatientRepository patientRepository) {
+        this.patientRepository = patientRepository;
+    }
+
+    @Transactional
+    public Patient getPatientById(Long id){
+         
+        Patient p1 = patientRepository.findById(id).orElseThrow(
+                () -> new RuntimeException("Patient not found with id: "+ id)
+        );
+
+//        Patient p2 = patientRepository.findById(id).orElseThrow();
+
+//        p1.setName("YoYo");
 
         return p1;
     }
--- src/test/java/com/codingshuttle/youtube/hospitalManagement/PatientTests.java
+++ src/test/java/com/codingshuttle/youtube/hospitalManagement/PatientTests.java
@@ -2,6 +2,7 @@ package com.codingshuttle.youtube.hospitalManagement;
 
 import com.codingshuttle.youtube.hospitalManagement.entity.Patient;
 import com.codingshuttle.youtube.hospitalManagement.repository.PatientRepository;
+import com.codingshuttle.youtube.hospitalManagement.service.PatientService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,9 +15,21 @@ public class PatientTests {
     @Autowired
     private PatientRepository patientRepository;
 
+    @Autowired
+    private PatientService patientService;
+
     @Test
     public void testPatientRepository() {
         List<Patient> patientList = patientRepository.findAll();
-        System.out.println(patientList);
+        System.out.println(patientList + "checks");
+    }
+
+    @Test
+    public void testTransactionMethod() {
+//        Patient patient = patientService.getPatientById(1L);
+
+        Patient patient = patientRepository.findByName("Diya Patel");
+
+        System.out.println(patient);
     }
 }
