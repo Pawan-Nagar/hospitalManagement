66de0b4f300dccd06bfc3df5c5f94845fd89441b
pawan <pa211nagar@gmail.com>
2025-07-28 23:58:27 +0530

    Adding new feature

---
+++ src/main/java/com/codingshuttle/youtube/hospitalManagement/entity/Appointment.java
@@ -0,0 +1,26 @@
+package com.codingshuttle.youtube.hospitalManagement.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+public class Appointment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDateTime appointmentTime;
+
+    @Column(length = 500)
+    private String reason;
+}
